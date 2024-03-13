577ff62991354178371a3c226e204e5d5280eeaf
aliyevvalii66 <aliyevvalii66@gmail.com>
2024-03-14 00:46:21 +0400

    -

---
+++ CustomSearch/Extension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomSearch
+{
+    public static class Extension
+    {
+        public static void CustomSort(this int[] numbers)
+        {
+            int temp;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CustomSearch/Program.cs
@@ -0,0 +1,56 @@
+﻿using CustomSearch;
+
+
+Console.Write("Arrayın uzunluğunu daxil edin : ");
+int size = int.Parse(Console.ReadLine());
+int[] array = new int[size];
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{i+1})Eded daxil edin : ");
+    array[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("");
+Console.WriteLine("------------------------------------------------");
+array.CustomSort(); //Array sort olunur
+string sortArray = "";
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == array.Length - 1) sortArray += $"{array[i].ToString()}";
+    else sortArray += $"{array[i].ToString()} , ";
+}
+Console.WriteLine(sortArray);
+
+
+Console.Write("Arrayda axtarmaq istediyiniz ededi daxil edin : ");
+int searchNumber = int.Parse(Console.ReadLine());
+int result = BinarySearch(array , 0,array.Length -1  , searchNumber); //Search methodundan qayidan deyer resula menimsedilir
+Console.WriteLine("");
+Console.WriteLine("------------------------------------------------");
+
+if (result != -1) Console.WriteLine($"Ededin indexi ---> {result}");
+else Console.WriteLine($"{searchNumber} ededi daxil edilen arrayda tapilmadi.");
+
+
+static int BinarySearch(int[] numbers, int firstIndex, int lastIndex, int searchNumber)
+{   
+    if (firstIndex > lastIndex) return -1;
+
+    int middleIndex = (firstIndex + lastIndex) / 2;
+
+    if (numbers[middleIndex] > searchNumber)
+    {
+        return BinarySearch(numbers, firstIndex, middleIndex - 1, searchNumber);
+    }
+    else if (numbers[middleIndex] < searchNumber)
+    {
+        return BinarySearch(numbers, middleIndex + 1, lastIndex, searchNumber);
+    }
+    else
+    {
+        return middleIndex;
+    }
+}
+
+
+
+Console.ReadKey();
